de95d4a67cf9eaabc23d967fea4764f8b28fbb7c
leedawnn <2g_hee@naver.com>
2023-03-22 21:15:35 +0900

    quick start

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import MyButton from '../src/components/MyButton';
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const handleCountUpBtn = () => {
+    setCount((prev) => prev + 1);
+  };
+
   return (
     <div>
-      <MyButton />
-      <MyButton />
-      <MyButton />
+      <MyButton count={count} handleCountUpBtn={handleCountUpBtn} />
+      <MyButton count={count} handleCountUpBtn={handleCountUpBtn} />
+      <MyButton count={count} handleCountUpBtn={handleCountUpBtn} />
     </div>
   );
 }
 export default App;
+s;
--- src/components/MyButton.tsx
+++ src/components/MyButton.tsx
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, { useEffect, useId } from 'react';
 
-function MyButton() {
-  return <button>I'm a button</button>;
+type MyButtonProps = {
+  count: number;
+  handleCountUpBtn: () => void;
+};
+
+function MyButton({ count, handleCountUpBtn }: MyButtonProps) {
+  const id = useId();
+  console.log(id);
+
+  useEffect(() => {
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+  }, [count]);
+
+  return <button onClick={handleCountUpBtn}>Clicked {count} times</button>;
 }
 
 export default MyButton;
